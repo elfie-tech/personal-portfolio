89e18a3fa5ab09ca45a2177fa5e343bc0b1f6691
Ellie Hansen <elliehansen11ads@gmail.com>
2021-03-15 22:01:48 -0600

    changed starwars folder to films folder

---
+++ films/main.js
@@ -0,0 +1,32 @@
+import { films } from '../data/films.js';
+
+const message = "If you can see this message, you be smart!";
+
+console.log(message);
+
+/* let itemOne = document.querySelector("#item1");
+let itemTwo = document.querySelector("#item2");
+
+itemOne.textContent = films[2].title;
+itemTwo.textContent = films[1].title; */
+
+let movieList = document.querySelector(".movieList");
+
+for (let i = 0; i < films.length; i++) {
+    const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1))
+    let figure = document.createElement("figure")
+    let newImage = document.createElement("img")
+    let figCaption = document.createElement("figcaption")
+    newImage.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+    figCaption.textContent = foundFilm.title
+
+    figure.appendChild(newImage)
+    figure.appendChild(figCaption)
+    movieList.append(figure)
+}
+
+function getLastNumber(url) {
+    let end = url[url.length - 2]
+    return parseInt(end, 10)
+}
+
