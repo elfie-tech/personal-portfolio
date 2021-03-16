e3b87878e29a579d1dc95a93f315fa6797edfe05
Ellie Hansen <elliehansen11ads@gmail.com>
2021-03-15 19:47:36 -0600

    added for loop

--- starwars/starwars.js
+++ starwars/starwars.js
@@ -4,5 +4,18 @@ const message = "If you can see this message, you be smart!";
 
 console.log(message);
 
-console.log(films);
+/* let itemOne = document.querySelector("#item1");
+let itemTwo = document.querySelector("#item2");
 
+itemOne.textContent = films[2].title;
+itemTwo.textContent = films[1].title; */
+
+let titleList = document.querySelector(".titleList");
+
+for (var i = 0; i < films.length; i++) {
+    let title = films[i].title
+    let newItem = document.createElement("li")
+    newItem.textContent = title
+    titleList.appendChild(newItem)
+
+}
