85b6dcf2ce65d11d55a411dd35456a4e980b2064
Ellie Hansen <elliehansen11ads@gmail.com>
2021-03-15 22:32:27 -0600

    added people loop

--- characters/main.js
+++ characters/main.js
@@ -1,2 +1,16 @@
 import { people } from "../data/people.js"
 
+const mainContent = document.querySelector("main")
+
+people.forEach(person => {
+    const charFigure = document.createElement("figure")
+    const charImg = document.createElement("img")
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+    const charCaption = document.createElement("figcaption")
+
+    charCaption.textContent = person.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
+})
