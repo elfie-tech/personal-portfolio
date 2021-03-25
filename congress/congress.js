e8d4c8cc701ec7074e2fdfb12640f2ec9829d803
Ellie Hansen <elliehansen11ads@gmail.com>
2021-03-25 12:45:40 -0600

    added showRepublicans function

--- congress/congress.js
+++ congress/congress.js
@@ -1,3 +1,22 @@
 import { senators } from "../data/senators.js"
 import { representatives } from "../data/representatives.js"
 
+const repubButton = document.querySelector("#republicans")
+
+repubButton.addEventListener("click", () => {
+    showRepublicans()
+})
+
+function showRepublicans() {
+    //const repubs = representatives.filter(rep => rep.party === 'R')
+    const repubs = representatives.map(rep => {
+        let smallRepub = {}
+        if (rep.party === "R") {
+            smallRepub.id = rep.id,
+            smallRepub.name = `${rep.first_name} ${rep.middle_name} ${rep.last_name}`
+        }
+        return smallRepub
+    })
+    console.log(repubs)
+}
+
