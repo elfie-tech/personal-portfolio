4728a9a716f42ff89c98b8a0f6558fbb5871d692
Ellie Hansen <elliehansen11ads@gmail.com>
2021-03-30 12:47:41 -0600

    added querySelectors and functions

--- congress/congress.js
+++ congress/congress.js
@@ -1,7 +1,45 @@
 import { senators } from "../data/senators.js"
 import { representatives } from "../data/representatives.js"
 
-const repubButton = document.querySelector("#republicans")
+const congressGrid = document.querySelector(".congressGrid")
+const seniorityButton = document.querySelector("#seniorityButton")
+const birthdayButton = document.querySelector("#birthdayButton")
+
+function populateCongressDiv(simplifiedList) {
+    simplifiedList.forEach(person => {
+        let personDiv = document.createElement("div")
+        let personFig = document.createElement("figure")
+        let figImg = document.createElement("img")
+        let figCaption = document.createElement("figcaption")
+
+        figImg.src = person.imgURL /* = `https:www.govtrack.us/static/legislator-photos/41295-100px.jpeg` */
+
+        figCaption.textContent = person.name
+
+
+        personFig.appendChild(figImg)
+        personFig.appendChild(figCaption)
+        personDiv.appendChild(personFig)
+        congressGrid.appendChild(personDiv)
+    })
+}
+
+function getSimplifiedPeople(PeopleList) {
+    return PeopleList.map(person => {
+        let middleName = person.middleName ? `${person.middle_name}` : `` 
+        return {
+            id: person.id,
+            name: `${person.first_name} ${middleName} ${person.last_name}`
+        }
+    })
+}
+
+populateCongressDiv(getSimplifiedPeople(representatives))
+
+
+
+
+/* const repubButton = document.querySelector("#republicans")
 
 repubButton.addEventListener("click", () => {
     showRepublicans()
@@ -20,3 +58,4 @@ function showRepublicans() {
     console.log(repubs)
 }
 
+ */
